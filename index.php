8cdfa810592467322ae772373275e4a7ac561fb1
TUSHAR122335 <71826453+TUSHAR122335@users.noreply.github.com>
2020-09-26 06:33:07 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>travel </title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto|Sriracha&display=swap" rel="stylesheet">
+		    <link rel="stylesheet" href="style.css">
+		</head>
+		<body>
+		    <img class="vv" src="vv.jpg" alt="vvvv" >
+
+		    <div class="container">
+		 <h1> WELCOME TO VIET AMRITSAR TRIP</h1>
+		<h3> ENTER YOUR DETAILS AND SUBMIT THIS FORM TO CONFIRM YOUR PARTICIPATION IN THE TRIP</h3>
+		<p class="submitmsg">Thanks for submitting your form. We are happy to to see your joining with us.</p>
+		<form action="index.php"method="post"></form>
+		<br>
+		<input type="text" name="name" id="name" placeholder="Enter your name"> 
+		<input type="age" name="age" id="gender" placeholder="Enter your age">
+		<input type="email" name="email" id="Email" placeholder="Enter your email">
+		<input type:"text" name="Phone" id="Phone"
+		placeholder="Phone">
+		<input type:"text" name="Address" id="Address"
+		placeholder="Address">
+		<textarea name="desc" id="desc" cols="30" rows="10"placeholder="Enter any other information here"></textarea>
+		<button class="btn">sumbit</button>
+	
+
+	</div>
+	<script src="index.js"></script>
+
+	</body>
+	
+</html>
