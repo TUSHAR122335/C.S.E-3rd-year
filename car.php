9dabfbe0f0910cf711198ff079b0f8637e574edf
TUSHAR122335 <71826453+TUSHAR122335@users.noreply.github.com>
2020-12-17 06:57:04 -0800

    Add files via upload

---
+++ car.php
@@ -0,0 +1,28 @@
+<!DOCTYPEhtml>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>car design template</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<section class="header"  id="header">
+		<div class="nav-bar">
+			<div class="logo">
+			<a href="#">Audi</a>
+			 </div>
+	 	</div>
+	<div class="menu">
+		<ul>
+			
+			<li><a href="#">cars</a></li>
+			<li><a href="#">History</a></li>
+			<li><a href="#">Technology</a></li>
+			<li><a href="#">production</a></li>
+			<li><a href="#">Reviews</a></li>
+
+			</ul>
+	</div>
+	</section>
+</body>
+</html>
