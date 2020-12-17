2975605a2cdd0d9afffabf89a4b8117ea266d630
Tushar Saxena <71825717+tusharsaxena01@users.noreply.github.com>
2020-12-17 22:53:18 +0530

    Update car.php

--- car.php
+++ car.php
@@ -1,26 +1,27 @@
-<!DOCTYPEhtml>
+<!-- Index1.html -->
 <html>
 <head>
-<meta charset="utf-8">
-<title>Car Design</title>
-<link rel="stylesheet" type="text/css" href="style.css" />
+	<title>Car Design</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css" />
 </head>
-<body>
-	<section id="header">
-		<div class="nav-bar">
-			<div class="logo">
-				<font size="20">Audi</font>
-			</div>
-			<div class="menu">
-				<ul align="right">
-					<li><a href="#">Cars</a></li>
-					<li><a href="#">History</a></li>
-					<li><a href="#">Technology</a></li>
-					<li><a href="#">Production</a></li>
-					<li><a href="#">Reviews</a></li>
-				</ul>
+	<body>
+	<div id=header style="background-color:royalblue;width:99%;height:15%;">
+	<img src="AUDI.jpg" align="left" alt="logo" height=105% border=0px>
+			<div class="nav">
+				<center>
+					<span class="MainText" style="text-transform:uppercase;font-size: 35px;">audi</span>
+				</center>
+					<center>
+						<div id="Menu" style="text-transform:uppercase;">
+							<a href="#" target="nav">cars</a>
+							<a href="#" target="nav">history</a>
+							<a href="#" target="nav">technology</a>
+							<a href="#" target="nav">production</a>
+							<a href="#" target="nav">reviews</a>
+						</div>
+					</center>
 			</div>
 		</div>
-	</section>
-</body>
+	</body>
 </html>
